4472b42cb81793b0049996fa4adfb24ac958bf9a
Abraham Fong <abisfong@ucsc.edu>
2022-02-05 16:41:56 -0800

    Only check if for bottom and top of element to verify it is on screen

--- js/main.js
+++ js/main.js
@@ -112,8 +112,6 @@ function elIsOnScreen(el) {
 
   return (
     rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
   );
 }
